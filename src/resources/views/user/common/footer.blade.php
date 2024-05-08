4c5862e4223257fad8ebbc6244dda4ce142557b5
zigzagdev <msts.oo0131@gmail.com>
2024-05-08 23:39:35 +0900

    email-backgroundcolor-change

--- src/resources/views/user/common/footer.blade.php
+++ src/resources/views/user/common/footer.blade.php
@@ -1,6 +1,12 @@
-<div>
-    <h1>Welcome to Our Service!</h1>
-    <p>We're glad to have you here.</p>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome Email</title>
+</head>
+<body style="background-color: #f8f9fa;">
+<h1>Welcome to Our Service!</h1>
+<p>We're glad to have you here.</p>
 
-    @include('user.common.footer')
-</div>
+@include('user.common.footer')
+</body>
+</html>
