d8b45e8fcf3071d0d332fc7a92461d907199da57
zigzagdev <msts.oo0131@gmail.com>
2024-05-08 23:37:31 +0900

    email-content-fix

--- src/resources/views/heritage/common/footer.blade.php
+++ src/resources/views/heritage/common/footer.blade.php
@@ -1,1 +1,5 @@
-<?php
+<div>
+    <h1>Thanks for registering the Heritage .</h1>
+    <p>This email has been sent from a send-only address.</p>
+    @include('heritage.common.footer')
+</div>
--- src/resources/views/user/common/footer.blade.php
+++ src/resources/views/user/common/footer.blade.php
@@ -1,1 +1,6 @@
-<?php
+<div>
+    <h1>Welcome to Our Service!</h1>
+    <p>We're glad to have you here.</p>
+
+    @include('user.common.footer')
+</div>
