f6ed7e82028f63c7a64b06a02a1cdf72380fcc9a
zigzagdev <msts.oo0131@gmail.com>
2024-05-12 13:16:23 +0900

    common-header&footer-fix

--- src/resources/views/heritage/common/footer.blade.php
+++ src/resources/views/heritage/common/footer.blade.php
@@ -1,5 +1,3 @@
-<div>
-    <h1>Thanks for registering the Heritage .</h1>
-    <p>This email has been sent from a send-only address.</p>
-    @include('heritage.common.footer')
-</div>
+==================
+All are reserved by Matthew
+==================
--- src/resources/views/user/common/footer.blade.php
+++ src/resources/views/user/common/footer.blade.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Welcome Email</title>
-</head>
-<body style="background-color: #f8f9fa;">
-<h1>Welcome to Our Service!</h1>
-<p>We're glad to have you here.</p>
-
-@include('user.common.footer')
-</body>
-</html>
+==================
+All are reserved by Happy Traveler inc .
+==================
 
