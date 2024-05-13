126d05d51cd571b6c5e46f834a588dff20dd4460
zigzagdev <msts.oo0131@gmail.com>
2024-05-14 00:42:38 +0900

    fix-blade-file

--- src/resources/views/user/create-user-email.blade.php
+++ src/resources/views/user/create-user-email.blade.php
@@ -1,8 +1,24 @@
 @include('common.header');
 
-<div style="background-color: #d9edf7">
-    Thank you for registering your data .
-</div>
+<html lang="en">
+  <style>
+      body {
+        background-color: #fffacd;
+      }
 
+      h1 {
+          font-size: 16px;
+          color: #ff6666;
+      }
+  </style>
+  <body>
+  <h1>
+      Registered Successfully Mail .
+  </h1>
+  <p>
+      Thank you for registering your data.
+  </p>
+  </body>
+</html>
 
 @include('common.footer');
