b8ec8144916ca937a07573b853d6ed358748c81c
zigzagdev <msts.oo0131@gmail.com>
2024-05-17 00:21:24 +0900

    add-error-handling-email

---
+++ src/resources/views/error/error-message.blade.php
@@ -0,0 +1,23 @@
+@include('user.common.header');
+
+<html lang="en">
+    <style>
+        body {
+            background-color: #fffacd;
+        }
+        h1 {
+        font-size: 16px;
+        color: #ff6666;
+        }
+    </style>
+    <body>
+        <h1>
+            Title: Now is under menitenance
+        </h1>
+        <p>
+            Sorry for inconvenience. Access to this page is temporarily disabled.
+        </p>
+    </body>
+</html>
+
+@include('user.common.footer');
