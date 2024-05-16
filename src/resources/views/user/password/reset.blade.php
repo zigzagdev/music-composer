e85d5949cbeebbb0597c949c37744fa7d25d8e96
zigzagdev <msts.oo0131@gmail.com>
2024-05-17 01:33:53 +0900

    add-reset-password-email

---
+++ src/resources/views/user/password/reset.blade.php
@@ -0,0 +1,53 @@
+@include('header');
+
+<html lang="en">
+    <style>
+        body {
+            background-color: #fffacd;
+        }
+        h1 {
+        font-size: 16px;
+        color: #ff6666;
+        }
+    </style>
+
+</html>
+
+@include('footer');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
