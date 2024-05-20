664d88e8ec035e6f8e36a3bf173bfdb07ba037e8
zigzagdev <msts.oo0131@gmail.com>
2024-05-21 02:06:31 +0900

    blade-file-fix

--- src/resources/views/user/password/reset-success.blade.php
+++ src/resources/views/user/password/reset-success.blade.php
@@ -1,16 +1,23 @@
 @include('header');
 
 <html lang="en">
-<style>
-    body {
-        background-color: #fffacd;
-    }
-    h1 {
-        font-size: 16px;
-        color: #ff6666;
-    }
-</style>
-
+    <style>
+        body {
+            background-color: #fffacd;
+        }
+        h1 {
+            font-size: 16px;
+            color: #ff6666;
+        }
+    </style>
+    <body>
+        <h1>
+            Reset Password Successfully .
+        </h1>
+        <p>
+            Your password has been reset successfully.
+        </p>
+    </body>
 </html>
 
 @include('footer');
