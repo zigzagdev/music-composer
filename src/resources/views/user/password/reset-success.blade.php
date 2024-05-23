d0402f3bd8a7d25055cc5ea06fbc1293254e19d1
zigzagdev <msts.oo0131@gmail.com>
2024-05-23 16:25:36 +0900

    mail-content-fix

--- src/resources/views/user/password/reset-success.blade.php
+++ src/resources/views/user/password/reset-success.blade.php
@@ -95,12 +95,6 @@
         <p style="color: #4a5568">
             Your password has been reset successfully.
         </p>
-        <p style="color: #4a5568">
-            Your password has been reset successfully.
-        </p>
-        <p style="color: #4a5568">
-            Your password has been reset successfully.
-        </p>
     </body>
 </html>
 
